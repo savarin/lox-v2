c686ad0c8a2e7dbee8c2c8a733352f86c9083948
Ezzeri Esa <ezzeri.esa@gmail.com>
2020-10-31 00:48:23 -0700

    up to 17.6

--- src/compiler.py
+++ src/compiler.py
@@ -1,5 +1,5 @@
 import enum
-from typing import Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import chunk
 import scanner
@@ -7,8 +7,39 @@ import value
 
 UINT8_MAX = 256
 
-
 # yapf: disable
+rule_map = {
+    "TOKEN_LEFT_PAREN":    ["grouping", None,     "PREC_NONE"],
+    "TOKEN_RIGHT_PAREN":   [None,       None,     "PREC_NONE"],
+    "TOKEN_LEFT_BRACE":    [None,       None,     "PREC_NONE"],
+    "TOKEN_RIGHT_BRACE":   [None,       None,     "PREC_NONE"],
+    "TOKEN_MINUS":         ["unary",    "binary", "PREC_TERM"],
+    "TOKEN_PLUS":          [None,       "binary", "PREC_TERM"],
+    "TOKEN_SEMICOLON":     [None,       None,     "PREC_NONE"],
+    "TOKEN_SLASH":         [None,       "binary", "PREC_FACTOR"],
+    "TOKEN_STAR":          [None,       "binary", "PREC_FACTOR"],
+    "TOKEN_BANG":          [None,       None,     "PREC_NONE"],
+    "TOKEN_BANG_EQUAL":    [None,       None,     "PREC_NONE"],
+    "TOKEN_EQUAL":         [None,       None,     "PREC_NONE"],
+    "TOKEN_EQUAL_EQUAL":   [None,       None,     "PREC_NONE"],
+    "TOKEN_GREATER":       [None,       None,     "PREC_NONE"],
+    "TOKEN_GREATER_EQUAL": [None,       None,     "PREC_NONE"],
+    "TOKEN_LESS":          [None,       None,     "PREC_NONE"],
+    "TOKEN_LESS_EQUAL":    [None,       None,     "PREC_NONE"],
+    "TOKEN_IDENTIFIER":    [None,       None,     "PREC_NONE"],
+    "TOKEN_STRING":        [None,       None,     "PREC_NONE"],
+    "TOKEN_NUMBER":        ["number",   None,     "PREC_NONE"],
+    "TOKEN_FALSE":         [None,       None,     "PREC_NONE"],
+    "TOKEN_FUN":           [None,       None,     "PREC_NONE"],
+    "TOKEN_PRINT":         [None,       None,     "PREC_NONE"],
+    "TOKEN_RETURN":        [None,       None,     "PREC_NONE"],
+    "TOKEN_TRUE":          [None,       None,     "PREC_NONE"],
+    "TOKEN_VAR":           [None,       None,     "PREC_NONE"],
+    "TOKEN_ERROR":         [None,       None,     "PREC_NONE"],
+    "TOKEN_EOF":           [None,       None,     "PREC_NONE"],
+}  # type: Dict[str, List[Optional[str]]]
+
+
 class Precedence(enum.Enum):
     PREC_NONE = 1
     PREC_ASSIGNMENT = 2  # =
@@ -20,6 +51,15 @@ class Precedence(enum.Enum):
     PREC_CALL = 8        # ()
     PREC_PRIMARY = 9
 # yapf: enable
+
+
+class ParseRule():
+    def __init__(self, prefix, infix, precedence):
+        #
+        """Wrapper for precedence rule."""
+        self.prefix = prefix
+        self.infix = infix
+        self.precedence = precedence
 
 
 class Parser():
@@ -170,7 +210,7 @@ def binary(viewer):
     operator_type = viewer.previous.token_type
 
     # Compile the right operand.
-    rule = get_rule(operator_type)
+    rule = get_rule(viewer, operator_type)
 
     # Get precedence which has 1 priority level above precedence of current rule
     precedence = Precedence(rule.precedence.value + 1)
@@ -230,7 +270,48 @@ def parse_precedence(viewer, precedence):
     # type: (Parser, Precedence) -> None
     """Starts at current token and parses expression at given precedence level
     or higher."""
-    pass
+    viewer = advance(viewer)
+
+    assert viewer.previous is not None
+    prefix_rule = get_rule(viewer, viewer.previous.token_type).prefix
+
+    if prefix_rule is None:
+        error(viewer, "Expect expression")
+        return None
+
+    prefix_rule(viewer)
+
+    assert viewer.current is not None
+    while precedence.value <= get_rule(viewer, viewer.current.token_type).precedence.value:
+        viewer = advance(viewer)
+
+        assert viewer.previous is not None
+        infix_rule = get_rule(viewer, viewer.previous.token_type).infix
+
+        infix_rule(viewer)
+
+
+def get_rule(viewer, token_type):
+    # type: (Parser, scanner.TokenType) -> ParseRule
+    """Custom function to convert TokenType to ParseRule. This allows the
+    rule_map to consist of strings, which are then replaced by respective
+    classes in the conversion process.
+    """
+    type_map = {
+        "binary": binary,
+        "grouping": grouping,
+        "number": number,
+        "unary": unary,
+    }
+
+    rule = rule_map[token_type.name]
+
+    assert rule[2] is not None
+    return ParseRule(
+        prefix=None if rule[0] is None else type_map[rule[0]],
+        infix=None if rule[1] is None else type_map[rule[1]],
+        precedence=Precedence[rule[2]],
+    )
 
 
 def compile(source, bytecode):
