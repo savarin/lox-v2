07a9dbbe37ec72f822fb76f6c21d6a7a143efe56
Ezzeri Esa <ezzeri.esa@gmail.com>
2020-11-02 19:14:37 -0800

    scaffolding for 24.2

--- src/function.py
+++ src/function.py
@@ -1,10 +1,20 @@
+import enum
+
 import chunk
+
+
+class FunctionType(enum.Enum):
+    """
+    """
+    TYPE_FUNCTION = "TYPE_FUNCTION"
+    TYPE_SCRIPT = "TYPE_SCRIPT"
 
 
 class Function():
     def __init__(self):
         # type: () -> None
         """Stores function bytecode with name and arity."""
+        self.function_type = None
         self.arity = 0
         self.bytecode = None
         self.name = None
@@ -14,6 +24,7 @@ def init_function():
     # type: () -> Function
     """Initialize new function."""
     function = Function()
+    function.function_type = FunctionType.TYPE_SCRIPT
     function.bytecode = chunk.init_chunk()
 
     return function
